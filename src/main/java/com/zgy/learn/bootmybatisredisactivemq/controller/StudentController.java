e475c4003568c478f6b4159e2f413e4371e04b26
zuiguangyin <2247359268@qq.com>
2020-04-21 01:43:25 +0800

    Student Controller

---
+++ src/main/java/com/zgy/learn/bootmybatisredisactivemq/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.zgy.learn.bootmybatisredisactivemq.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.zgy.learn.bootmybatisredisactivemq.service.StudentService;
+import com.zgy.learn.bootmybatisredisactivemq.utils.JSONUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/4/21 1:01
+ * @Modified by:
+ */
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @RequestMapping("getonestudent")
+    @ResponseBody
+    public String getonestudent(Integer stId) throws JsonProcessingException {
+        return JSONUtil.getJsonFromObject(service.getStudentById(stId));
+    }
+
+    @RequestMapping("getallstudent")
+    @ResponseBody
+    public String getallstudent() throws JsonProcessingException {
+        return JSONUtil.getJsonFromObject(service.getAllStudents());
+    }
+}
