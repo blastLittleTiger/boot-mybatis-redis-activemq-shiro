c645ca74f26721facfe05e3546e9e2f823d1e3f6
zuiguangyin <2247359268@qq.com>
2020-04-21 01:43:46 +0800

    扫描Mapper的包

--- src/main/java/com/zgy/learn/bootmybatisredisactivemq/BootMybatisRedisActivemqApplication.java
+++ src/main/java/com/zgy/learn/bootmybatisredisactivemq/BootMybatisRedisActivemqApplication.java
@@ -1,13 +1,15 @@
 package com.zgy.learn.bootmybatisredisactivemq;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 扫描Mapper包
+@MapperScan(basePackages = {"com.zgy.learn.bootmybatisredisactivemq.mapper"})
 @SpringBootApplication
 public class BootMybatisRedisActivemqApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BootMybatisRedisActivemqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisRedisActivemqApplication.class, args);
+    }
 
 }
