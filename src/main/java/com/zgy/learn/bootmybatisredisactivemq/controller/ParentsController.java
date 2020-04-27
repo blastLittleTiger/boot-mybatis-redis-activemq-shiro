2bbc8c628660d8fe22f02c47503fa1720259c8de
zuiguangyin <2247359268@qq.com>
2020-04-28 00:01:08 +0800

    在增加和删除之中使用了缓存操作

--- src/main/java/com/zgy/learn/bootmybatisredisactivemq/controller/ParentsController.java
+++ src/main/java/com/zgy/learn/bootmybatisredisactivemq/controller/ParentsController.java
@@ -1,7 +1,9 @@
 package com.zgy.learn.bootmybatisredisactivemq.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.zgy.learn.bootmybatisredisactivemq.pojo.Parents;
 import com.zgy.learn.bootmybatisredisactivemq.service.ParentsService;
+import com.zgy.learn.bootmybatisredisactivemq.service.RedisService;
 import com.zgy.learn.bootmybatisredisactivemq.utils.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,9 @@ public class ParentsController {
     @Autowired
     ParentsService service;
 
+    @Autowired
+    private RedisService redisService;
+
     @GetMapping("getparents")
     public String getParentsById(Integer parentsId) throws JsonProcessingException {
         return JSONUtil.getJsonFromObject(service.getParentsById(parentsId));
@@ -28,13 +33,30 @@ public class ParentsController {
         Map<String, Object> map = new HashMap<>();
         map.put("fatherName", fatherName);
         map.put("motherName", motherName);
-        return JSONUtil.getJsonFromObject(service.addParents(map));
-
+        // 添加到缓存之中， 简单使用
+        int result = service.addParents(map);
+        if (result == 1) {
+            Parents p = service.getParentsByFatherMother(fatherName, motherName);
+            redisService.set(String.valueOf(p.getParentsId()), p);
+            return JSONUtil.getJsonFromObject("添加用户成功，已经添加到缓存之中!");
+        } else if (result == 0) {
+            return JSONUtil.getJsonFromObject("添加用户失败!");
+        } else {
+            return JSONUtil.getJsonFromObject("发生错误!");
+        }
     }
 
     @PostMapping("delete")
     public String deleteParents(Integer parentId) throws JsonProcessingException {
-        return JSONUtil.getJsonFromObject(service.deleteParents(parentId));
+        if (redisService.hasKey(String.valueOf(parentId))) {
+            // 先删除redis缓存，然后删除数据库值
+            redisService.del(String.valueOf(parentId));
+            service.deleteParents(parentId);
+            return JSONUtil.getJsonFromObject("删除redis缓存, 删除了数据库值!");
+        } else {
+            service.deleteParents(parentId);
+            return JSONUtil.getJsonFromObject("删除了数据库值!");
+        }
     }
 
     @PostMapping("update")
