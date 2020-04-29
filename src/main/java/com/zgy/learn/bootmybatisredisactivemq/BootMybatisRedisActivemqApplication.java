0bfe8236518d402862df0d55d47e1277e2940c81
zuiguangyin <2247359268@qq.com>
2020-04-30 00:15:43 +0800

    开启MQ的支持

--- src/main/java/com/zgy/learn/bootmybatisredisactivemq/BootMybatisRedisActivemqApplication.java
+++ src/main/java/com/zgy/learn/bootmybatisredisactivemq/BootMybatisRedisActivemqApplication.java
@@ -3,10 +3,13 @@ package com.zgy.learn.bootmybatisredisactivemq;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 // 扫描Mapper包
 @MapperScan(basePackages = {"com.zgy.learn.bootmybatisredisactivemq.mapper"})
 @SpringBootApplication
+// 开启JMS支持，此处是ActiveMq
+@EnableJms
 public class BootMybatisRedisActivemqApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootMybatisRedisActivemqApplication.class, args);
